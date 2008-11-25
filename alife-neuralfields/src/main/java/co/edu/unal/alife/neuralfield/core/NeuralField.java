1591e49c2626060c949b62d6d2eb19634e9a4b99
jjfigueredou <jjfigueredou@a98b334e-663c-4a39-a41c-f2b0a0f92540>
2008-11-25 00:39:15 +0000

    Cambios pom y NeuralField

--- alife-neuralfields/src/main/java/co/edu/unal/alife/neuralfield/core/NeuralField.java
+++ alife-neuralfields/src/main/java/co/edu/unal/alife/neuralfield/core/NeuralField.java
@@ -8,7 +8,7 @@ import java.util.List;
 import co.edu.unal.alife.dynamic.core.Simulable;
 
 /**
- * @author jjfigueredou
+ * @author Juan Figueredo
  *
  */
 public abstract class NeuralField<K,V> implements Simulable {
