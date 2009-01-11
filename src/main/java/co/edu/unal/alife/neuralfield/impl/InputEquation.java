9119c29c79c26e591fd91a206cb27cc402ac5c52
jjfigueredou <jjfigueredou@a98b334e-663c-4a39-a41c-f2b0a0f92540>
2009-01-11 23:45:08 +0000

    Se incluye ponderación de u

--- src/main/java/co/edu/unal/alife/neuralfield/impl/InputEquation.java
+++ src/main/java/co/edu/unal/alife/neuralfield/impl/InputEquation.java
@@ -67,7 +67,7 @@ public class InputEquation implements DeltaEquation<Double> {
 			if (position.equals(eqPosition)) {
 				newPopulation.setElementValue(position, 1.0);
 			} else if (position.equals(eqPosition2)) {
-				newPopulation.setElementValue(position, 0.9);
+				newPopulation.setElementValue(position, 0.8);
 			} else {
 				newPopulation.setElementValue(position, 0.0);
 
