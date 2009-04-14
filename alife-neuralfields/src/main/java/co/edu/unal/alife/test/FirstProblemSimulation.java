151dba3dc8feffa7313924f2cc4737eee573120c
jjfigueredou <jjfigueredou@a98b334e-663c-4a39-a41c-f2b0a0f92540>
2009-04-14 09:17:04 +0000

    Configuración para impresión pendulum thetas gecco

--- alife-neuralfields/src/main/java/co/edu/unal/alife/test/FirstProblemSimulation.java
+++ alife-neuralfields/src/main/java/co/edu/unal/alife/test/FirstProblemSimulation.java
@@ -11,17 +11,17 @@ import co.edu.unal.alife.dynamics.AbstractSolver;
 import co.edu.unal.alife.dynamics.DeltaPopulation;
 import co.edu.unal.alife.dynamics.RungeKutta4thSolver;
 import co.edu.unal.alife.evolution.ParameterizedKernel;
-import co.edu.unal.alife.evolution.PendulumControllerParameters;
 import co.edu.unal.alife.neuralfield.DeltaEquation;
 import co.edu.unal.alife.neuralfield.DeltaField;
 import co.edu.unal.alife.neuralfield.KernelFunction;
 import co.edu.unal.alife.neuralfield.impl.InputEquation;
 import co.edu.unal.alife.neuralfield.impl.MapDeltaPopulation;
-import co.edu.unal.alife.neuralfield.impl.MexicanHatKernel;
 import co.edu.unal.alife.neuralfield.impl.SimpleDeltaField;
 import co.edu.unal.alife.neuralfield.impl.SimpleEquation;
 import co.edu.unal.alife.output.PendulumFrame;
+import co.edu.unal.alife.output.PendulumPrinter;
 import co.edu.unal.alife.output.Tracer;
+import co.edu.unal.alife.output.Visualizer;
 import co.edu.unal.alife.pendulum.PendulumEquation;
 
 /**
@@ -56,26 +56,26 @@ public class FirstProblemSimulation {
 		List<KernelFunction> firstRow = new ArrayList<KernelFunction>(N);
 		List<KernelFunction> secondRow = new ArrayList<KernelFunction>(N);
 		List<KernelFunction> thirdRow = null;
-		KernelFunction inputKernelFunction = new MexicanHatKernel(0.10,2,2.50);
-		KernelFunction selfKernelFunction = new MexicanHatKernel(0.10,2,0.30);
+//		KernelFunction inputKernelFunction = new MexicanHatKernel(0.10,2,2.50);
+//		KernelFunction selfKernelFunction = new MexicanHatKernel(0.10,2,0.30);
 		
-//
-//		Double[] inputArray = new Double[] { 0.8, -0.4, 0.3821521867217581, 0.8,
-//				0.6627201010260433, 0.8, 0.5996071258752236, 0.4310174353169244, 0.8, 0.8,
-//				-0.0016068530503658973, 0.8, 0.8, -0.15603003061895004, 0.3911531992201058,
-//				-0.019875594601236057, 0.8, -0.22352170644144687, -0.4, -0.3057861650728686,
-//				0.3263381733053502 };
-//		Double[] processingArray = new Double[] { -0.4, -0.4, 0.7966620749205899, -0.4, -0.4,
-//				0.5584618779825862, 0.19059994140172853, -0.19807857514308644, 0.6213276196314829,
-//				0.1208663553191761, 0.4785335021631033, -0.05444274298155438, -0.289283934994388,
-//				0.6395960411516046, 0.3399083060933949, 0.07606263739663152, -0.2750226685836359,
-//				0.8, -0.1987387459605986, 0.8, 0.02725196339176175 };
-		
-//		List<Double> inputList = Arrays.asList(inputArray);
-//		List<Double> processingList = Arrays.asList(processingArray);
 
-//		KernelFunction inputKernelFunction = new ParameterizedKernel(inputList);
-//		KernelFunction selfKernelFunction = new ParameterizedKernel(processingList);
+		Double[] inputArray = new Double[] { 0.8, -0.4, 0.3821521867217581, 0.8,
+				0.6627201010260433, 0.8, 0.5996071258752236, 0.4310174353169244, 0.8, 0.8,
+				-0.0016068530503658973, 0.8, 0.8, -0.15603003061895004, 0.3911531992201058,
+				-0.019875594601236057, 0.8, -0.22352170644144687, -0.4, -0.3057861650728686,
+				0.3263381733053502 };
+		Double[] processingArray = new Double[] { -0.4, -0.4, 0.7966620749205899, -0.4, -0.4,
+				0.5584618779825862, 0.19059994140172853, -0.19807857514308644, 0.6213276196314829,
+				0.1208663553191761, 0.4785335021631033, -0.05444274298155438, -0.289283934994388,
+				0.6395960411516046, 0.3399083060933949, 0.07606263739663152, -0.2750226685836359,
+				0.8, -0.1987387459605986, 0.8, 0.02725196339176175 };
+		
+		List<Double> inputList = Arrays.asList(inputArray);
+		List<Double> processingList = Arrays.asList(processingArray);
+
+		KernelFunction inputKernelFunction = new ParameterizedKernel(inputList);
+		KernelFunction selfKernelFunction = new ParameterizedKernel(processingList);
 
 		firstRow.add(null); // self-connectivity of input field
 		firstRow.add(null); // connectivity from output to input
@@ -103,9 +103,9 @@ public class FirstProblemSimulation {
 		DeltaField<Double> field = new SimpleDeltaField(equations, kernelMatrix, populations,
 				solver);
 
-		// Visualizer printer = new PendulumPrinter(pendulumStates, mainSize, mainSize);
+//		Visualizer printer = new PendulumPrinter(pendulumStates, mainSize, mainSize);
 		Tracer tracer = new Tracer(N);
-		// field.addObserver(printer);
+//		field.addObserver(printer);
 		field.addObserver(tracer);
 
 		// Run simulation
@@ -114,8 +114,8 @@ public class FirstProblemSimulation {
 //		String[] filenames = {"inputPopulation","fieldPopulation","pendulum"};
 //		String[] filenames = { "inputPopulation_old", "fieldPopulation_old", "pendulum_old" };
 //		tracer.printToFiles(filenames);
-//		String[] filenames = { null, null, "pendulum_ijcnn" };
-		String[] filenames = { "inputPopulation_ijcnn","fieldPopulation_ijcnn",null};
+		String[] filenames = { null, null, "pendulum_gecco" };
+//		String[] filenames = { "inputPopulation_ijcnn","fieldPopulation_ijcnn",null};
 		tracer.printToFiles(filenames);
 		
 		// Run animation
